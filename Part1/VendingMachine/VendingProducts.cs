b30789544b3642d7375c8cd175c91895a2f2d80c
VetleB <vetle.birkeland@gmail.com>
2024-02-15 18:24:57 +0100

    Add product list

---
+++ Part1/VendingMachine/VendingProducts.cs
@@ -0,0 +1,16 @@
+public class VendingProducts 
+{
+    public static List<Product> products = 
+    [
+        new Product("Coke", 20),
+        new Product("Snickers", 25),
+        new Product("Water", 10),
+        new Product("Pepsi", 20),
+        new Product("Fanta", 15),
+        new Product("Kvikk Lunsj", 25),
+        new Product("Solo", 20),
+        new Product("Twix", 30),
+        new Product("Sprite", 20),
+        new Product("M&Ms", 30),
+    ];
+}
