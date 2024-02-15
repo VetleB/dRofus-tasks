d35089610a891ad539a12873015193ae0b22f357
VetleB <vetle.birkeland@gmail.com>
2024-02-15 18:25:53 +0100

    Run simple loop, add list command

--- Part1/VendingMachine/Program.cs
+++ Part1/VendingMachine/Program.cs
@@ -2,12 +2,16 @@
 
 while (!quit) 
 {
-    var vendingMachine = new VendingMachine();
+    var products = VendingProducts.products;
+    var vendingMachine = new VendingMachine(products);
 
     var userInput = Console.ReadLine()?.ToLower();
-    
+
     switch (userInput)
     {
+        case "list":
+            Console.WriteLine("< " + vendingMachine.ListProducts());
+            break;
         case "q":
             quit = true;
             break;
--- Part1/VendingMachine/VendingMachine.cs
+++ Part1/VendingMachine/VendingMachine.cs
@@ -1,9 +1,31 @@
 public class VendingMachine
 {
     private int money;
+    private Dictionary<string, Product> stock = new Dictionary<string, Product>();
 
-    public VendingMachine() 
+    public VendingMachine(List<Product> products) 
     {
         this.money = 0;
+        this.StockProducts(products);
+    }
+
+    private void StockProducts(List<Product> products)
+    {
+        foreach (var product in products) 
+        {
+            this.stock.Add(product.Name, product);
+        }
+    }
+
+    public string ListProducts()
+    {
+        var outString = "";
+
+        foreach (var product in stock.Values)
+        {
+            outString += $"{product.Name} - {product.Price}, ";
+        }
+
+        return outString.Substring(0, outString.Length - 2);
     }
 }
