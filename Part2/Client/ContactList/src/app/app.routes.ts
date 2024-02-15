a3525528ac9da3a487b1f4bb31f15c8231c72c27
VetleB <vetle.birkeland@gmail.com>
2024-02-16 00:08:22 +0100

    Initialize web app

---
+++ Part2/Client/ContactList/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactDetailComponent } from './contact-detail/contact-detail.component';
+
+export const routes: Routes = [
+    { path: '', redirectTo: 'contact-list', pathMatch: 'full' },
+    { path: 'contact-list', component: ContactListComponent, pathMatch: 'full' },
+    { path: 'contact-detail', component: ContactDetailComponent, pathMatch: 'full' },
+];
